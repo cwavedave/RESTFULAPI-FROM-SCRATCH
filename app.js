cd9f66d656668f927ce47219911b279a4ae45619
cwavedave <david@creativewavelength.co.uk>
2020-02-12 22:32:29 +0100

    Code succesfully sends stored collections when route is requested (GET)

--- app.js
+++ app.js
@@ -11,14 +11,29 @@ app.use(bodyParser.urlencoded({
   extended:true
 }));
 
-mongoose.connect('mongodb://localhost:270137/wikiDB',{
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+const articleSchema =  {
+  title: String,
+  content: String
+};
 
+const Article = mongoose.model("Article", articleSchema);
 
+app.get("/articles", function(req, res) {
+  Article.find(function(err, foundArticles) {
+    if(!err) {
+      console.log("200");
+      res.send(foundArticles);
+    } else {
+      console.log("Something went wrong");
+    }
+  });
+});
 
-app.listen(3000,function() {
-  console.log("App is running on Port 3000");
-})
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
